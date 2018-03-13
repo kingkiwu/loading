20d3ef84a35bf445be706e9cee4e3907fdcf57dd
sbd_dev3 <sbd_dev3>
2018-03-13 11:42:00 +0800

    页面加载动画

---
+++ loading/js/index.js
@@ -0,0 +1,16 @@
+/* 
+* @Author: Marte
+* @Date:   2018-03-09 11:57:05
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-03-13 11:10:09
+*/
+$(".fakeloader").fakeLoader({  
+         timeToHide: 1200,  
+         bgColor: "rgba(0,67,86,0.2)",  
+         spinner: "spinner2"  
+     }); 
+$(".fakeloader").fakeLoader({  
+        timeToHide: 1,  
+        bgColor: "rgba(0,67,86,0.2)",  
+        spinner: "spinner2"  
+    }); 
